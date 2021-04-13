02e709740d386ab2db1a3cd7416d5084f8e97558
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-04-13 20:46:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/oop23032021/Animal.java
@@ -0,0 +1,11 @@
+package com.example.oop23032021;
+
+public class Animal {
+    // thuộc tính
+    String name;
+    String color;
+
+    // phương thức
+
+}
+
---
+++ app/src/main/java/com/example/oop23032021/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.oop23032021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Person;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Object , class
+
+        // object đại diện cho 1 thực thể
+        // class đại diện cho 1 quần thể
+
+        // Class => Kiểu dữ liệu
+
+        Animal meo = new Animal();
+        meo.color = "Xám";
+        meo.name = "Mi mi";
+
+        Log.d("BBB",meo.name);
+
+    }
+}
