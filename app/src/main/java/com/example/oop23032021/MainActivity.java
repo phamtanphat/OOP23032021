edc6044f73a8e058eb75cbfbbeccdbd0a1a9d4ff
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-04-15 20:09:50 +0700

    overload

--- app/src/main/java/com/example/oop23032021/MainActivity.java
+++ app/src/main/java/com/example/oop23032021/MainActivity.java
@@ -24,12 +24,23 @@ public class MainActivity extends AppCompatActivity {
 //
         Cat meo = new Cat("mimi" , "Xám");
         meo.setName("Milu");
-        Log.d("BBB",meo.getName());
+//        Log.d("BBB",meo.getName());
 
         // Tính bao đóng
         // Tính kế thừa
         // Tính đa hình
         // Tính trừu tượng
+    // overload : phương thức nạp chồng
+//        1 : Các phương thức nạp chồng cùng nằm trên 1 vị trí
+//        2 : Các phương thức nạp chồng phải khác với nhau về tham số truyền vào
+        printMessage("Xin Chào");
+        printMessage(android.R.id.message);
+    }
+    private void printMessage(String message){
+        Log.d("BBB",message);
+    }
+    private void printMessage(Integer resId){
+        Log.d("BBB",String.valueOf(resId));
     }
 
 }
